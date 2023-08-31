e12784d1f83a9f7613fae67e9a5e7680644a2e32
Haavaala <haavaala@stud.ntnu.no>
2023-08-31 15:49:43 +0200

    created navbar not functional tho

---
+++ vuetail/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+    <h1 class="text-6xl font-bold  flex justify-center mt-5">
+      Hello darkness my old friend!
+    </h1>
+
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navigation.vue'
+
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
---
+++ vuetail/components/navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="bg-blue-500 py-4">
+    <ul class="flex justify-center space-x-16">
+      <li>
+        <nuxt-link to="/" class="text-white hover:underline">Halla</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/wallah" class="text-white hover:underline">Wallah</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/inshallah" class="text-white hover:underline">Inshallah</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/bro" class="text-white hover:underline">Bro</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/wakkaflow" class="text-white hover:underline">Wakkaflow</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
+
